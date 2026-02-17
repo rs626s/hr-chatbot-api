559636995dad1e630449a5790a66ff9a71211f26
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-16 19:20:58 -0600

    Add files via upload

--- src/main/java/edu/missouristate/csc615/chatbot/repository/UserRepository.java
+++ src/main/java/edu/missouristate/csc615/chatbot/repository/UserRepository.java
@@ -2,12 +2,16 @@ package edu.missouristate.csc615.chatbot.repository;
 
 import edu.missouristate.csc615.chatbot.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 
     boolean existsByUsername(String username);
 
