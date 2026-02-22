b90864588945dce786dae4d7b067052e2872c2cf
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-21 22:53:22 -0600

    Add files via upload

--- src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
+++ src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
@@ -2,8 +2,10 @@ package edu.missouristate.csc615.chatbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HrChatbotApplicationTests {
 
     @Test
